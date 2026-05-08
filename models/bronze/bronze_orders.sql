d9e47da79b147a70f4acc7f641bf8b8a668e6129
Tanvi Tanvi <tchoudhary84@yahoo.com>
2026-05-08 08:17:29 +0000

    added incremental

--- models/bronze/bronze_orders.sql
+++ models/bronze/bronze_orders.sql
@@ -1,4 +1,17 @@
+{{
+    config(
+        materialized = 'incremental',
+        unique_key= 'id'
+    )
+}}
+
 select 
 *
 FROM 
 {{source('landing','orders')}}
+
+{% if is_incremental() %}
+
+where created_at > (select coalesce(max(created_at,'1990-01-01')))
+
+{% endif%}
